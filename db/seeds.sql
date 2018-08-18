2f00f8d5b59fa3f8284009b846682c1b2fceac5b
Victoria Gonzalez <vgonzalez3092@gmail.com>
2018-08-18 14:05:59 -0400

    folder setup

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Vegan Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon and Cheese Burger');
